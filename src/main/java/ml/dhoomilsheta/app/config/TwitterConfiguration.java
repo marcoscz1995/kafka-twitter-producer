5f548d9dbe166aab6201a3aab34c1422eaee48a8
Marcos <43050697+marcoscz1995@users.noreply.github.com>
2019-05-21 14:53:29 -0400

    Update TwitterConfiguration.java

--- src/main/java/ml/dhoomilsheta/app/config/TwitterConfiguration.java
+++ src/main/java/ml/dhoomilsheta/app/config/TwitterConfiguration.java
@@ -1,9 +1,9 @@
 package ml.dhoomilsheta.app.config;
 
 public class TwitterConfiguration {
-    public static final String CONSUMER_KEY = "PbHuTRuSqSHTUzAJGJuqZAZoz";
-    public static final String CONSUMER_SECRET = "BjpUlQLT7lVuXR7kI0PJCYS77b6hFDuQKYLLex7ETfKRYDiLpX";
-    public static final String ACCESS_TOKEN = "319641898-eSXscJnl6aTyExpEZUCVi6RyQ8JzRPIQ4R9qFDA5";
-    public static final String TOKEN_SECRET = "1AYcrO4wodtFxVZKWv5UB5v7eKgZq21wJ8xaTRYkvhCKU";
-    public static final String HASHTAG = "#bigdata";
+    public static final String CONSUMER_KEY = "qRUwa8kt4v39ifPnSkAkdIe23";
+    public static final String CONSUMER_SECRET = "5TYzBkj7298tdKIMcItnL0gM1LZwDxREID33Czd4MxrOL9bp90";
+    public static final String ACCESS_TOKEN = "1130529007521456129-jDUQ51pNxAmgCTGqGgM80lqsKdhZG3";
+    public static final String TOKEN_SECRET = "c32GMpk3FvafqA7ceZuAOeNmXfMuAR9CIDjVAvPlFj6rc";
+    public static final String HASHTAG = "#honda";
 }
